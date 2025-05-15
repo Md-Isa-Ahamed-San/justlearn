b85346eba2c58eecc5ccda4fbf9c03884f13e962
Md-Isa-Ahamed-San <glordisagtav@gmail.com>
2025-05-15 12:19:03 +0600

    gg'

--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,19 @@
 import { Toaster } from "@/components/ui/sonner";
 import { cn } from "@/lib/utils";
-// import { Inter } from "next/font/google";
-// import { Delius } from "next/font/google";
+import { Inter } from "next/font/google";
+import { Delius } from "next/font/google";
 
-// import Head from "next/head"; // ✅ Import Head
+import Head from "next/head"; // ✅ Import Head
 import "./globals.css";
 
-// const inter = Inter({ subsets: ["latin"] });
-// const poppins = Inter({ subsets: ["latin"], variable: "--font-poppins" });
+const inter = Inter({ subsets: ["latin"] });
+const poppins = Inter({ subsets: ["latin"], variable: "--font-poppins" });
 
-// const delius = Delius({
-//   subsets: ["latin"],
-//   weight: "400",
-//   variable: "--font-delius",
-// });
+const delius = Delius({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-delius",
+});
 export const metadata = {
   title: "JUSTLearn",
   description: "Explore || Learn || Build || Share",
@@ -23,10 +23,17 @@ export default async function RootLayout({ children }) {
   // const conn = await dbConnect();
   return (
     <html lang="en" className="dark">
-     
-      <body 
-      // className={cn(inter.className, poppins.className, delius.variable)}
-      >
+      <Head>
+      
+        <link
+          rel="preload"
+          as="fetch"
+          href="/hero-animation.json"
+          type="application/json"
+          crossOrigin="anonymous"
+        />
+      </Head>
+      <body className={cn(inter.className, poppins.className, delius.variable)}>
         {children}
         <Toaster richColors position="top-center" />
       </body>
